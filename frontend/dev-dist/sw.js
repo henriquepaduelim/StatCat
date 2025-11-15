66e827e68dc47484895fee48232298f39d0f4104
Henrique Padueli <henriquepaduelim@icloud.com>
2025-11-15 11:12:47 -0300

    black pwa icon

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.3bu8jitkdvo"
+    "revision": "0.783g8qfacdg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
